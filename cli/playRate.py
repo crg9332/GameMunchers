5453f57b53df9d235b07518d788c621c350da7d6
Patricio Solis <101016848+PatoSolis02@users.noreply.github.com>
2023-03-10 10:44:39 -0500

    Initial Implementation of rating a game

---
+++ cli/playRate.py
@@ -0,0 +1,37 @@
+
+
+def rate(curs, username, args):
+    if len(args) != 2:
+        print("Usage: rate <game> <rating>")
+        return
+    if not args[1].isdigit():
+        print("Rating must be a number ")
+        return
+    game = args[0]
+    rate = int(args[1])
+    if rate < 0 or rate > 5:
+        print("Rating must be in range 0-5")
+        return
+    try:
+        curs.execute("SELECT gameid FROM games WHERE gameTitle = %s", game)
+        gameid = curs.fetchall()
+        if len(gameid) == 0:
+            print("Game does not exist")
+            return
+        curs.execute("SELECT gameid FROM inCollection where gameid = %d AND username = %s", gameid[0][0], username)
+        checkOwned = curs.fetchall()
+        if len(checkOwned) == 0:
+            print("Game is not owned")
+            return
+        curs.execute("SELECT gameid FROM starrating where gameid = %d AND username = %s", gameid[0][0], username)
+        checkReviewed = curs.fetchall()
+        if len(checkReviewed) == 0:
+            curs.exectue("INSERT INTO starratin VALUES (%s, %d, %d, NOW())", username, gameid[0][0], rate)
+            print("New rating done!")
+            return
+        curs.execute("UPDATE starrating SET starrating = %d WHERE gameid = %d AND username = %s", rate, gameid[0][0], username)
+        print("Rating updated!")
+        return
+    except Exception as e:
+        print(e)
+        curs.execute("ROLLBACK")
