65c98d51ad7c30867fcbbc98dc4c3c9ee6468172
pgs5983 <pgs5983@rit.edu>
2023-03-10 17:25:25 -0500

    Added the methods to play a random or chosen game.

--- cli/playRate.py
+++ cli/playRate.py
@@ -1,3 +1,6 @@
+from random import *
+from datetime import datetime, timedelta, time
+
 def rate(curs, username, args):
     if len(args) != 2:
         print("Usage: rate <game> <rating>")
@@ -39,3 +42,67 @@ def rate(curs, username, args):
     except Exception as e:
         print(e)
         curs.execute("ROLLBACK")
+
+
+# Play random game from collection
+def play(curs, username, args):
+    if len(args) != 1:
+        print("Usage: play <gameTitle/random>")
+    if args[0] == "random":
+        playRandom(curs, username)
+    else:
+        playChosen(curs, username, args[0])
+
+    return
+
+def playRandom(curs, username):
+    try:
+        # Get all games owned by user
+        curs.execute("SELECT gameid FROM incollection WHERE username = %s", (username,))
+        gamesOwned = curs.fetchall()
+        
+        # Get a random game id from previous query's results
+        random.seed(None)
+        randomGame = gamesOwned[random.randint(0, len(gamesOwned))]
+        curs.execute("SELECT gameTitle FROM games WHERE gameid = %s", (randomGame[0],))
+        gameTitle = curs.fetchall()
+
+        # Prompt user for amount of timeplayed and calculate end dateTime
+        timePlayed = input("How long do you want to play {0} for (in minutes)? ".format(gameTitle))
+        startDateTime = datetime.now()
+        endDateTime = startDateTime + timedelta(minutes=timePlayed)
+
+        # Insert new game session into appropriate table
+        curs.execute("INSERT INTO gamesession (username, gameid, startdatetime, enddatetime VALUES (%s, %s, %s, %s)", (username, randomGame[0], startDateTime, endDateTime))
+        output = "{0} has been played for {0} minutes!".format(gameTitle, timePlayed)
+        print(output)
+    except Exception as e:
+        print(e)
+        curs.execute("ROLLBACK")
+    return
+
+# Play chosen game from collection
+def playChosen(curs, username, gameTitle):
+    try:
+        # Get game id with the provided game title
+        curs.execute("SELECT gameid FROM games as g, incollection as in WHERE gameTitle = %s AND g.gameid = in.gameid AND in.username = %s", (gameTitle, username))
+        gameId = curs.fetchone()
+
+        # Check if user owns given game
+        if len(gameId) == 0:
+            print("You do not own {0}".format(gameTitle))
+            return
+        
+        # Prompt user for amount of timeplayed and calculate end dateTime
+        timePlayed = input("How long do you want to play {0} for (in minutes)? ".format(gameTitle))
+        startDateTime = datetime.now()
+        endDateTime = startDateTime + timedelta(minutes=timePlayed)
+
+        # Insert new game session into appropriate table
+        curs.execute("INSERT INTO gamesession (username, gameid, startdatetime, enddatetime VALUES (%s, %s, %s, %s)", (username, gameId[0], startDateTime, endDateTime))
+        output = "{0} has been played for {0} minutes!".format(gameTitle, timePlayed)
+        print(output)
+    except Exception as e:
+        print(e)
+        curs.execute("ROLLBACK")
+    return
